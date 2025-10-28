b328588068d65293bafa90adb6006c4c7b5d73f2
harsh <harshkr.agrl@gmail.com>
2025-10-28 14:02:07 +0530

    initialized

---
+++ src/app.js
@@ -0,0 +1,15 @@
+const express = require("express");
+const app = express();
+
+app.use("/home", (req, res) => {
+  res.send("Hello home page!");
+});
+app.use("/login", (req, res) => {
+  res.send("Login done right!");
+});
+app.use("/", (req, res) => {
+  res.send("Hello harsh!");
+});
+app.listen(3000, () => {
+  console.log("Server is running at port 3000");
+});
