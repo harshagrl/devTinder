b89517f3cf0bcba3aa2b533116afef8427fda928
harsh <harshkr.agrl@gmail.com>
2025-11-15 20:17:45 +0530

    changes in auth file

--- src/middlewares/auth.js
+++ src/middlewares/auth.js
@@ -4,7 +4,7 @@ const userAuth = async (req, res, next) => {
   try {
     const { token } = req.cookies;
     if (!token) {
-      return res.status(401).send("Unauthorized: No token provided");
+      return res.status(401).send("Please Login!");
     }
 
     const decoded = jwt.verify(token, process.env.JWT_SECRET);
