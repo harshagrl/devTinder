ce886eef65de64dbf062d0884df04197ba525222
harsh <harshkr.agrl@gmail.com>
2025-11-24 18:15:42 +0530

    some changes

--- src/routers/authRouter.js
+++ src/routers/authRouter.js
@@ -15,8 +15,12 @@ authRouter.post("/signup", async (req, res) => {
       email,
       password: hashedPassword,
     });
-    await user.save();
-    res.send("User added successfully");
+    const savedUser = await user.save();
+    const token = await savedUser.getJWT();
+    res.cookie("token", token, {
+      expires: new Date(Date.now() + 8 * 3600000),
+    });
+    res.json({ message: "User added successfully", data: savedUser });
   } catch (err) {
     res.status(400).send("ERROR: " + err.message);
   }
