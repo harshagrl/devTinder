b2d4201ebd04e492b00d4db92cbc9d186a52d19f
harsh <harshkr.agrl@gmail.com>
2025-11-25 16:13:10 +0530

    removed env

--- src/config/database.js
+++ src/config/database.js
@@ -1,7 +1,9 @@
 require("dotenv").config();
 const mongoose = require("mongoose");
 const connectDB = async () => {
-  await mongoose.connect(process.env.MONGODB_URI);
+  await mongoose.connect(
+    "mongodb+srv://intellidocs:%40Intellidocs654%2F%2F@cluster0.oj9qf9p.mongodb.net/devTinder?retryWrites=true&w=majority&appName=Cluster0"
+  );
 };
 
 module.exports = connectDB;
